d387818707d8b722f38646d0c1d0e1ec178e1019
Fenriez <andy.dolot@gmail.com>
2022-04-13 17:50:31 +0300

    Iconic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import './App.css';
+import Note from "./components/Note";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App_container">
-        
+      <div className="App_content">
+        <section className="container notes">
+          <div className="container__header">
+            <div className="header__cell"></div>
+            <div className="header__cell">Name</div>
+            <div className="header__cell">Created</div>
+            <div className="header__cell">Category</div>
+            <div className="header__cell">Content</div>
+            <div className="header__cell">Dates</div>
+            <div className="header__cell">
+              <button id="archived_visibility" className="iconic_button">
+                <i className="fa-solid fa-box-archive"></i>
+              </button>
+              <button id="delete_all" className="iconic_button">
+                <i className="fa-solid fa-trash-can"></i>
+              </button>
+            </div>
+          </div>
+          <div className="container__body">
+            <Note />
+          </div>
+          <div className="container__footer">
+            <div className="footer__cell"></div>
+            <div className="footer__cell">
+              <button id="create_note" className="custom_btn">
+                Create note
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="container categories"></section>
       </div>
     </div>
   );
